ebba695d9b6d02e5773d24cabda77a304f78c09e
Andy <andy.10lin@hotmail.com>
2025-04-27 22:24:24 -0400

    test

--- discord_bot/slash_commands.py
+++ discord_bot/slash_commands.py
@@ -9,6 +9,4 @@ spotify_token, spotify_expiry = get_token.get_token()
 def setup(bot: commands.Bot, GUILD_ID) -> None:
     @bot.tree.command(name="amigo", description="Say hola", guild=GUILD_ID)
     async def hello(interaction: discord.Interaction):
-
         await interaction.response.send_message(user_functions.get_current_user(spotify_token),ephemeral=True)
-
