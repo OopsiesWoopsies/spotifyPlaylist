49af1044be7f7a32d862c4aa28862ea50687b6d0
Andy <andy.10lin@hotmail.com>
2025-05-03 00:02:38 -0400

    played with discord api

--- discord_bot/discord_bot.py
+++ discord_bot/discord_bot.py
@@ -5,19 +5,22 @@ import logging
 import os
 
 bot_token = os.getenv("DISCORD_BOT_TOKEN")
-GUILD_ID = discord.Object(id=1366142338342322397)
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.guilds = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
-    await bot.tree.sync(guild=GUILD_ID)
-    print(f"I, {bot.user.name}, am ready")
+    try:
+        bot.tree.add_command(slash_commands.User(name="user", description="User related commands"))
 
-slash_commands.setup(bot, GUILD_ID)
+        await bot.tree.sync(guild=slash_commands.GUILD_ID)
+        print(f"I, {bot.user.name}, am ready")
+    except Exception as e:
+        print(f"FAILED {e}")
 
 bot.run(bot_token, log_handler=handler, log_level=logging.DEBUG)
--- discord_bot/slash_commands.py
+++ discord_bot/slash_commands.py
@@ -1,14 +1,33 @@
+from discord.app_commands import guilds
+
 from spotify_util_functions import get_token, user_functions
 
 import discord
-from discord.ext import commands
+from discord import app_commands
+
 
 spotify_token, spotify_expiry = get_token.get_token()
+GUILD_ID = discord.Object(id=1366142338342322397)
 
 
-def setup(bot: commands.Bot, GUILD_ID) -> None:
-    @bot.tree.command(name="amigo", description="Say hola", guild=GUILD_ID)
-    async def hello(interaction: discord.Interaction):
-        user_id = interaction.user.id # check if id is in env file before printing (same for every user related slash command)
+class User(app_commands.Group):
+    @app_commands.command(name="current-user", description="Check yourself out!")
+    @app_commands.guilds(GUILD_ID)
+    async def current_user(self, interaction: discord.Interaction):
+        user_id = interaction.user.id  # check if id is in env file before printing (same for every user related slash command)
         print(user_id)
-        await interaction.response.send_message(user_functions.get_current_user(spotify_token),ephemeral=True)
+        await interaction.response.send_message(user_functions.get_current_user(spotify_token), ephemeral=True)
+
+# @bot.tree.command(name="current_user", description="Check yourself out!", guild=GUILD_ID)
+# async def current_user(interaction: discord.Interaction):
+#     user_id = interaction.user.id # check if id is in env file before printing (same for every user related slash command)
+#     print(user_id)
+#     await interaction.response.send_message(user_functions.get_current_user(spotify_token),ephemeral=True)
+#
+# @bot.command(name="account", description="", guild=GUILD_ID)
+# async def spotify_account(interaction: discord.Interaction, add: str):
+#     pass
+#
+# @spotify_account.Group(name="add", description="Add your spotify account!", guild=GUILD_ID)
+# async def add_spotify_account(interaction: discord.Interaction):
+#     pass
